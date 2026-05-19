1a225a94851bcbaa729dddee28a5d74928159c83
Mavour <mavourgaming@gmail.com>
2026-05-19 20:40:16 +0800

    Set default fee TVL floor to five

--- config.js
+++ config.js
@@ -193,7 +193,7 @@ export const config = {
     minVolumeToRebalance:  u.minVolumeToRebalance  ?? 1000,
     stopLossPct:           u.stopLossPct           ?? u.emergencyPriceDropPct ?? -50,
     takeProfitPct:         u.takeProfitPct         ?? u.takeProfitFeePct ?? 5,
-    minFeePerTvl24h:       u.minFeePerTvl24h       ?? 3,
+    minFeePerTvl24h:       u.minFeePerTvl24h       ?? 5,
     minAgeBeforeYieldCheck: u.minAgeBeforeYieldCheck ?? 60, // minutes before low yield can trigger close
     minSolToOpen:          u.minSolToOpen          ?? 0.55,
     deployAmountSol:       u.deployAmountSol       ?? 0.5,
